b87e17c7ddfe1a5399ecb82a2e4a9a68127e5218
EnhancedTarget <levente.kalman922@gmail.com>
2023-02-21 12:42:27 +0000

    Update main.c

--- week4/main.c
+++ week4/main.c
@@ -1,22 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct {
-	long bookNum;
+	int bookNum;
 	char title[30];
 	char author[25];
 	double price;
 }bookT;
 
-void readBooks(bookT* bookDetails, int size);
-void printBooks(bookT* bookDetails, int size);
-void searchBooks(bookT* bookDetails, int size);
+void addBook(bookT* bookDetails, int size);
+void displayBook(bookT* bookDetails, int size);
+void searchBook(bookT* bookDetails, int size);
 
 void main() {
-	int numOfBooks;
+	int numOfBooks = 0;
 	bookT* list;
+	int menuOpt = 0;
 
-	printf("Please enter the number of books in the list\n");
-	scanf("%d", &numOfBooks);
+	/*printf("Please enter the number of books in the list\n");
+	scanf("%d", &numOfBooks);*/
 
-	list = (bookT)*malloc(numOfBooks * sizeof(bookT));
+	//menu loop 
+	while (menuOpt == 0) {
+		printf("Please enter 1 to search a book, 2 to add a book, 3 to edit a book or -1 to Exit: ");
+		scanf("%d", &menuOpt);
+
+		if (menuOpt == 1) {
+
+		}
+		else if (menuOpt == 2) {
+
+		}
+		else if (menuOpt == 3) {
+
+		}
+		else if (menuOpt == -1) {
+			exit;
+		}
+		else {
+			printf("The number you have entered is invalid, please enter a correct option!\n");
+			menuOpt = 0;
+		}
+	}
+
+	list = (bookT*)malloc(numOfBooks * sizeof(bookT));
+
+	addBook(list, numOfBooks);
+
+	displayBook(list, numOfBooks);
+
+	searchBook(list, numOfBooks);
+}
+
+void addBook(bookT* bookDetails, int size) {
+	for (int i = 0; i < size; i++) {
+
+	}
+}
+
+void displayBook(bookT* bookDetails, int size) {
+
+}
+
+void searchBook(bookT* bookDetails, int size) {
+
 }
