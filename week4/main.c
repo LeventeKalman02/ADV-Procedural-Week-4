53350961c044d362d9c15271b229b9c87d8bf6dd
EnhancedTarget <levente.kalman922@gmail.com>
2023-03-13 14:00:07 +0000

    update

--- week4/main.c
+++ week4/main.c
@@ -1,60 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 typedef struct {
-	int bookNum = 0;
+	long bookNum;
 	char title[30];
 	char author[25];
 	double price;
 }bookT;
 
+void mainMenu(bookT* bookDetails, int size, int menuOpt, int numOfBooks);
 void addBook(bookT* bookDetails, int size);
 void displayBook(bookT* bookDetails, int size);
 void searchBook(bookT* bookDetails, int size);
 void editBook(bookT* bookDetails, int size);
 
 void main() {
-	int numOfBooks = 0;
-	bookT* list;
-	int menuOpt = 0;
+	int numOfBooks;
+	bookT* library;
+	int init;
+
+	mainMenu(library, numOfBooks);
+
+	//initialising the library
+	for (int i = 0; i < numOfBooks; i++) {
+		(library + i)->bookNum = 0;
+		strcpy((library + i)->title, "");
+		strcpy((library + i)->author, "");
+		(library + i)->price = 0;
+	}
 
 	/*printf("Please enter the number of books in the list\n");
 	scanf("%d", &numOfBooks);*/
 
+
+	library = (bookT*)malloc(numOfBooks * sizeof(bookT));
+
+	
+	//calling functions
+	displayBook(library, numOfBooks);
+
+}
+
+void mainMenu(bookT* bookDetails, int size, int menuOpt, int numOfBooks) {
+
+
 	//menu loop 
-	while (menuOpt == 0) {
+	while ( menuOpt =! -1) {
 		printf("Please enter 1 to search a book, 2 to add a book, 3 to edit a book or -1 to Exit: ");
 		scanf("%d", &menuOpt);
 
 		if (menuOpt == 1) {
-			searchBook(list, numOfBooks);
+			searchBook(bookDetails, numOfBooks);
 		}
 		else if (menuOpt == 2) {
-			addBook(list, numOfBooks);
+			addBook(bookDetails, numOfBooks);
 		}
 		else if (menuOpt == 3) {
-			editBook(list, numOfBooks);
-		}
-		else if (menuOpt == -1) {
-			exit;
+			editBook(bookDetails, numOfBooks);
 		}
 		else {
 			printf("The number you have entered is invalid, please enter a correct option!\n");
 			menuOpt = 0;
 		}
-	}
 
-	list = (bookT*)malloc(numOfBooks * sizeof(bookT));
-
-	
-	//calling functions
-	displayBook(list, numOfBooks);
-
+		printf("Please enter 1 to search a book, 2 to add a book, 3 to edit a book or -1 to Exit: ");
 }
 
 void addBook(bookT* bookDetails, int size) {
 
 	for (int i = 0; i < size; i++) {
-
+		
 	}
 }
 
