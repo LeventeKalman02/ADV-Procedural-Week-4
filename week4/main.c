5d46d72bc443c0afdd1f450d93349a54f3372708
EnhancedTarget <levente.kalman922@gmail.com>
2023-03-14 11:59:17 +0000

    addBook

--- week4/main.c
+++ week4/main.c
@@ -9,7 +9,7 @@ typedef struct {
 	double price;
 }bookT;
 
-void mainMenu(bookT* bookDetails, int size, int menuOpt, int numOfBooks);
+void mainMenu(bookT* bookDetails, int size);
 void addBook(bookT* bookDetails, int size);
 void displayBook(bookT* bookDetails, int size);
 void searchBook(bookT* bookDetails, int size);
@@ -20,7 +20,9 @@ void main() {
 	bookT* library;
 	int init;
 
-	mainMenu(library, numOfBooks);
+	library = (bookT*)malloc(numOfBooks * sizeof(bookT));
+
+	mainMenu(library, numOfBooks);	
 
 	//initialising the library
 	for (int i = 0; i < numOfBooks; i++) {
@@ -28,13 +30,7 @@ void main() {
 		strcpy((library + i)->title, "");
 		strcpy((library + i)->author, "");
 		(library + i)->price = 0;
-	}
-
-	/*printf("Please enter the number of books in the list\n");
-	scanf("%d", &numOfBooks);*/
-
-
-	library = (bookT*)malloc(numOfBooks * sizeof(bookT));
+	}	
 
 	
 	//calling functions
@@ -42,22 +38,22 @@ void main() {
 
 }
 
-void mainMenu(bookT* bookDetails, int size, int menuOpt, int numOfBooks) {
-
+void mainMenu(bookT* bookDetails, int size) {
+	int menuOpt;
 
 	//menu loop 
-	while ( menuOpt =! -1) {
+	while (menuOpt = !- 1) {
 		printf("Please enter 1 to search a book, 2 to add a book, 3 to edit a book or -1 to Exit: ");
 		scanf("%d", &menuOpt);
 
 		if (menuOpt == 1) {
-			searchBook(bookDetails, numOfBooks);
+			searchBook(bookDetails, size);
 		}
 		else if (menuOpt == 2) {
-			addBook(bookDetails, numOfBooks);
+			addBook(bookDetails, size);
 		}
 		else if (menuOpt == 3) {
-			editBook(bookDetails, numOfBooks);
+			editBook(bookDetails, size);
 		}
 		else {
 			printf("The number you have entered is invalid, please enter a correct option!\n");
@@ -65,12 +61,32 @@ void mainMenu(bookT* bookDetails, int size, int menuOpt, int numOfBooks) {
 		}
 
 		printf("Please enter 1 to search a book, 2 to add a book, 3 to edit a book or -1 to Exit: ");
+	}
 }
 
 void addBook(bookT* bookDetails, int size) {
-
+	int addBook = 0;
 	for (int i = 0; i < size; i++) {
-		
+		if ((bookDetails + i)->bookNum == 0) {
+			printf("Please enter the book number\n");
+			scanf("%ld", &(bookDetails + i)->bookNum);
+
+			printf("Please enter the book name\n");
+			scanf("%s", (bookDetails + i)->title);
+
+			printf("Please enter the book author\n");
+			scanf("%s", (bookDetails + i)->author);
+
+			printf("Please enter the price\n");
+			scanf("%lf", &(bookDetails + i)->price);
+			addBook = 1;
+			break;
+		}
+
+		// give error if library is full
+		if (addBook == 0) {
+			printf("Sorry the library is full\n");
+		}
 	}
 }
 
